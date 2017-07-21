2049b5ea17606511825321cb4b39955d1b35d3c1
McPrapor <max.terleckiy@gmail.com>
2017-07-22 01:25:03 +0200

    Update droid-hcismd-up.sh

--- sparse/usr/bin/droid/droid-hcismd-up.sh
+++ sparse/usr/bin/droid/droid-hcismd-up.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-# transport is smd
-setprop ro.qualcomm.bt.hci_transport smd
-setprop ro.bt.bdaddr_path /data/misc/bluetooth/bdaddr
-setprop net.bt.name Android
-
-# initialise chip and board-address with correct mac
-BLUETOOTH_MAC=`/system/bin/btnvtool -p 2>&1| sed -n 's/ //g;s/.*://;s/\./\:/g;y/abcdef/ABCDEF/;1p'`
-echo Setting bluetooth address to $BLUETOOTH_MAC | systemd-cat -p info -t "droid-hcismd-up.sh"
-
-echo $BLUETOOTH_MAC > /var/lib/bluetooth/board-address
-/system/bin/hci_qcomm_init -d /dev/ttysmd3 -s 3000000 -i 115200 -r 32000000 -p 0 -P 1 -b $BLUETOOTH_MAC
-
 # Maximum number of attempts to enable hcismd to try to get
 # hci0 to come online.  Writing to sysfs too early seems to
 # not work, so we loop.
 MAXTRIES=15
 
-seq 1 $MAXTRIES | while read i ; do
+#setprop bluetooth.hciattach true
+setprop ro.qualcomm.bt.hci_transport smd
+setprop ro.bt.bdaddr_path /data/misc/bluetooth/bdaddr
+setprop net.bt.name Android
+setprop qcom.bt.dev_power_class 2
+setprop qcom.bt.le_dev_pwr_class 2
+
+i=1
+while [ ! $i -gt $MAXTRIES ]  ; do
+    rfkill unblock all
     echo 1 > /sys/module/hci_smd/parameters/hcismd_set
     if [ -e /sys/class/bluetooth/hci0 ] ; then
+        rfkill unblock all
+        hciconfig hci0 up
         # found hci0, exit successfully
+        echo 0 > /sys/module/hci_smd/parameters/hcismd_set
+        bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>1 | grep -oP '([0-9a-f]{2}:){5}([0-9a-f]{2})')
+        echo "BT MAC: $bt_mac"
+        if [ ! -z "$bt_mac" ] ; then
+            echo $bt_mac > /var/lib/bluetooth/board-address
+            echo "BT MAC: $bt_mac"
+        fi
+        echo 1 > /sys/module/hci_smd/parameters/hcismd_set
         exit 0
     fi
     sleep 1
@@ -29,3 +36,5 @@ seq 1 $MAXTRIES | while read i ; do
         exit 1
     fi
 done
+rfkill unblock all
+hciconfig hci0 up
