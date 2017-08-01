040ed7289e0f1f3d42dd7c8ba3eeab12b6878b07
McPrapor <max.terleckiy@gmail.com>
2017-08-01 13:21:13 +0200

    Create shim.sh

---
+++ sparse/etc/profile.d/shim.sh
@@ -0,0 +1,2 @@
+export HYBRIS_LD_SHIM_LIBS="/system/lib/libcald_pal.so|libshim_cald.so:/system/lib/hw/camera.vendor.qcom.so|libshim_camera.so:/system/lib/libMPU3050.so|libshim_MPU3050.so"
+export LD_SHIM_LIBS="/system/lib/libcald_pal.so|libshim_cald.so:/system/lib/hw/camera.vendor.qcom.so|libshim_camera.so:/system/lib/libMPU3050.so|libshim_MPU3050.so"
